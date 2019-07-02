3cdd347a59f815d27e0e7ccc2ee5616c4bdb1c70
thehopeofjoe <30753872+thehopeofjoe@users.noreply.github.com>
2019-07-02 14:40:50 +1200

    Add project files and folders

---
+++ my-app/src/App.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+
+import DropArea from './Components/DropArea/DropArea'
+import Header from './Components/Header/Header'
+import Result from './Components/Result/Result'
+
+interface IState{
+    result:string
+    filelength:number
+}
+
+
+class App extends React.Component<{},IState {
+    public constructor(props:any){
+        super(props)
+        this.state = {
+            result:"",
+            filelength:0,
+        }
+    }
+  public render() {
+    return (
+      <div>
+        <Header />
+        <DropArea setResults = {this.resultState} />
+        <Result = result = {this.state.result} filelength = {this.state.filelength} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ my-app/src/Components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import * as React from 'React'
+import './Header.css'
+
+export default function Header(){
+    return(
+        <div className = 'header'>
+            <h1 className = 'headerText'> How old? 🤔 </h1>
+        </div>
+    )
+}
